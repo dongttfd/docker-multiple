fbc4852be4e6be58a5f280d0b0ce5d21c70757b9
Trần Trọng Đông <dongtt@dongtt-macmini.local>
2020-12-28 11:22:25 +0700

    update db config

--- etc/sql/01.sql
+++ etc/sql/01.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `gossby` CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
-GRANT ALL ON `gossby`.* TO 'dongtt'@'%';
-CREATE DATABASE IF NOT EXISTS `teawork` CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
-GRANT ALL ON `teawork`.* TO 'dongtt'@'%';
 CREATE DATABASE IF NOT EXISTS `wordpress` CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `teawork` CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+GRANT ALL ON `gossby`.* TO 'dongtt'@'%';
+GRANT ALL ON `teawork`.* TO 'dongtt'@'%';
 GRANT ALL ON `wordpress`.* TO 'dongtt'@'%';
+FLUSH PRIVILEGES;
